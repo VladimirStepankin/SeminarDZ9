eea7cab07a20e70bfcea1206cb751674a05a4229
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-12-12 14:33:42 +0300

    Добавил решение задачи № 68

---
+++ Exs68/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+ Даны два неотрицательных числа m и n.*/
+
+int m = ReadInt("Введите m: ");
+int n = ReadInt("Введите n: ");
+int Akkerman = FuncAkkerman(m, n);
+Console.WriteLine($"A({m},{n}) = {Akkerman}");
+int FuncAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if  ((n == 0) && (m > 0)) return FuncAkkerman(m - 1, 1);
+    else  return FuncAkkerman(m - 1, FuncAkkerman(m, n - 1));
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
