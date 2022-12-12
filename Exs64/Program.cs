f31a7884a3ebf4e3175eede5ca01acd9115aeb9a
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-12-12 13:59:03 +0300

    Добавил решение задачи № 64

---
+++ Exs64/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+int n = ReadInt("Введите N: ");
+Console.WriteLine(NaturalNumber(n));
+int NaturalNumber(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+    {
+        Console.Write(n + ",");
+        return NaturalNumber(n - 1);
+    }
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
